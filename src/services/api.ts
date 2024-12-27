8026a3d1f6c07a5b7412d11ff57eae4da26d37e5
Bruno Arruda <86083291+bruno-arruda-dev@users.noreply.github.com>
2024-12-26 21:23:20 -0300

    Implementa error interceptor

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { HandleSessionStorage } from '../utils/session-storage';
+import { toastError } from '../utils/toast-utils';
 
 const baseURL = import.meta.env.VITE_BASE_URL;
 const user = HandleSessionStorage.getUserData();
@@ -11,5 +12,16 @@ const Api = axios.create({
         'Authorization': `Bearer ${user?.token}`
     }
 });
+
+Api.interceptors.response.use(
+    response => response,
+    error => {
+        if (!error.response) {
+            toastError('Ocorreu algum problema. Por favor tente denovo mais tarde');
+        }
+
+        return Promise.reject(error);
+    }
+);
 
 export default Api;
